2e827e79ec5001cf3f4ea889101863f1beb62921
Luckily45 <kumarsinghamit904@gmail.com>
2024-05-25 02:30:03 +0530

    new changes

--- src/pages/page.ts
+++
@@ -1,6 +0,0 @@
-import About from "./About";
-import Contact from "./Contact";
-import HomePage from "./HomePage";
-import Services from "./Services";
-
-export { About, Contact, HomePage, Services }
